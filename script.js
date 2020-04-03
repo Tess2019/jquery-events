2f4329a6ea529e2a824cc38e95be4a2bb820b2ca
Tess2019 <terese_burman@hotmail.com>
2020-04-03 15:38:27 +0000

    jquery functions

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {
  
-
+//looking at writing jquery effects
  	$("#stream1_btn").on("click", function() {
- 		$(".stream1").removeClass('highlight_stream');
-		$(".stream2").removeClass('highlight_stream');
-		$(".stream3").removeClass('highlight_stream');
-	  	$(".stream1").addClass('highlight_stream');
+ 		$(".stream1").slideToggle();
+		$(".stream2").fadeIn("slow");
+		$(".stream3").fadeOut("medium");
+          $(".stream1").fadeToggle("fast");
+          	$(".stream1").fadeTo(1000, 0,5);
 	});
 	$("#stream2_btn").on("click", function() {
 		$(".stream1").removeClass('highlight_stream');
